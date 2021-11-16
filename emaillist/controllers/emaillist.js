c0417497d3a0f81a7a9a2b6339d47b2c5820d245
yangsw3114 <72145880+yangsw3114@users.noreply.github.com>
2021-11-16 16:43:28 +0900

    emaillist

--- emaillist/controllers/emaillist.js
+++ emaillist/controllers/emaillist.js
@@ -1,11 +1,13 @@
 
 const model = require('../models/emaillist');
 
+//여기서 async와 await 의 역할 중요
 module.exports = {
-    index: function(req, res) {
-        const result = model.findAll();
+    index: async function(req, res) { 
+        const results = await model.findAll(function(){});
+        console.log(results);
         res.render('index', {
-            list: result || []
+            list: results || []
         });
     }
 }
--- emaillist/models/emaillist.js
+++ emaillist/models/emaillist.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql2');
+const util = require('util');
 
 module.exports = {
-    findAll: function() {
+    findAll: async function(callback) {
         const conn = mysql.createConnection({
             host: '127.0.0.1',
             port: 3306,
@@ -9,7 +10,25 @@ module.exports = {
             password: 'webdb',
             database: 'webdb' 
         });
-    }
+        
+        // const query = function(sql, data) {
+        //     return new Promise(function(resolve, reject){
+        //         conn.query(sql, [], function(error, results, field){
+        //             return error ? reject(error) : resolve(results);
+        //         })
+        //     })
+        // }
 
-  //  conn.query(sql, data)
+        // const query = (sql, data) => new Promise((resolve, reject) => conn.query(sql, [], (error, results, field) => (error ? reject(error) : resolve(results))))
+        
+        const query = util.promisify(conn.query).bind(conn);
+
+        try {
+            return await query('select no, first_name as firstName, last_name as lastName, email from emaillist order by no desc', []);
+        } catch(e) {
+            console.error(e);
+        } finally {
+            conn.end();
+        }
+    }
 }
